92f07552f4ac80425c27927e2d5d88fe40166e29
jyotsnayadagiri <99063521+jyotsnayadagiri@users.noreply.github.com>
2025-08-22 02:20:00 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,3 @@
-output "vm_instance_name" {
-  description = "Name of the VM instance"
-  value       = module.gcp_infra.vm_instance_name
-}
-
-output "vm_instance_ip" {
-  description = "Public IP address of the VM instance"
-  value       = module.gcp_infra.vm_instance_ip
-}
-
-output "network_name" {
-  description = "Name of the VPC network"
-  value       = module.gcp_infra.network_name
-}
-
-output "firewall_name" {
-  description = "Name of the firewall rule"
-  value       = module.gcp_infra.firewall_name
-}
 
 output "bucket_name" {
   description = "Name of the GCS bucket"
@@ -37,3 +18,4 @@ output "application_url" {
   description = "URL to access the Todo application"
   value       = "http://${module.gcp_infra.vm_instance_ip}:8080"
 }
+
